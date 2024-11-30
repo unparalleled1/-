35c718da0bb1fa668135006a3c4022aeed3624f5
unparalleled1 <2807289863@qq.com>
2024-12-01 01:04:03 +0800

    Revert "1"
    This reverts commit 7109e2c68806097a4a49704946840dbcbb824040.

--- USER/main.c
+++ USER/main.c
@@ -5,7 +5,7 @@
 
 #include "MPU6050.h"
 #include "inv_mpu.h"
-#include "ad.h" sssssssssssssss 11
+#include "ad.h" sssssssssssssss 
 float Pitch,Roll,Yaw;	
 int16_t ax,ay,az,gx,gy,gz;	
 float anglea;
